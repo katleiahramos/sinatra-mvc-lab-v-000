5eb81e600cf3e3fbc73144502d4dc58128a9e938
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-21 17:32:25 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,9 @@
+def piglatin(word)
+  if word =~ (/\A[aeiou]/i)
+    word = word + 'ay'
+  elsif word =~ (/\A[^aeiou]/i)
+    match = /\A[^aeiou]/i.match(word)
+    word = match.post_match + match.to_s + 'ay'
+  end
+  word
+end
