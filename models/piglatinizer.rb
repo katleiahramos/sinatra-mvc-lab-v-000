c9f4376a2c5d6ceb21d4aed7a5a3f9939e0d0949
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-21 17:41:11 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ def piglatinize(string)
   if vowels.include?(str[0])
     str + 'ay'
   elsif consonants.include?(str[0]) && consonants.include?(str[1])
-    str[2..-1] + str[0..1] + 'ay'
+    str[2..-1] + str[0..1] + 'way'
   elsif consonants.include?(str[0])
     str[1..-1] + str[0] + 'ay'
   else
