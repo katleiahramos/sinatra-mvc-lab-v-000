7e603dcdfb43c13d3df82ae93ef5436472d6113f
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-21 17:34:04 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,17 @@
-def piglatin(word)
-  if word =~ (/\A[aeiou]/i)
-    word = word + 'ay'
-  elsif word =~ (/\A[^aeiou]/i)
-    match = /\A[^aeiou]/i.match(word)
-    word = match.post_match + match.to_s + 'ay'
+def translate str
+  alpha = ('a'..'z').to_a
+  vowels = %w[a e i o u]
+  consonants = alpha - vowels
+
+  if vowels.include?(str[0])
+    str + 'ay'
+  elsif consonants.include?(str[0]) && consonants.include?(str[1])
+    str[2..-1] + str[0..1] + 'ay'
+  elsif consonants.include?(str[0])
+    str[1..-1] + str[0] + 'ay'
+  else
+    str # return unchanged
   end
-  word
 end
 
 binding.pry
